18b06cc7c389234668ebe6120874f2e84bd56db2
Mizar <mizar.jp@gmail.com>
2019-05-06 03:37:32 +0900

    - Makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -33,11 +33,16 @@ ARCH_DEF := -DTARGET_ARCH="$(TARGET_ARCH_ABI)"
 # example: 2018 T.N.K. (EVAL_NNUE)
 # $ ndk-build ENGINE_TARGET=YANEURAOU_2018_TNK_ENGINE
 
+# example: tnk- Mate (MATE_ENGINE)
+# $ ndk-build MATE_ENGINE
+
 ENGINE_TARGET := YANEURAOU_2018_OTAFUKU_ENGINE
 #ENGINE_TARGET := YANEURAOU_2018_OTAFUKU_ENGINE_KPPT
 #ENGINE_TARGET := YANEURAOU_2018_OTAFUKU_ENGINE_KPP_KKPT
 #ENGINE_TARGET := YANEURAOU_2018_OTAFUKU_ENGINE_MATERIAL
 #ENGINE_TARGET := YANEURAOU_2018_TNK_ENGINE
+#ENGINE_TARGET := YANEURAOU_2018_TNK_ENGINE_K_P
+#ENGINE_TARGET := MATE_ENGINE
 
 ifeq ($(ENGINE_TARGET),YANEURAOU_2018_OTAFUKU_ENGINE)
   ARCH_DEF += -DUSE_MAKEFILE -DYANEURAOU_2018_OTAFUKU_ENGINE
@@ -64,6 +69,16 @@ ifeq ($(ENGINE_TARGET),YANEURAOU_2018_TNK_ENGINE)
   ENGINE_NAME := YaneuraOu2018tnk
 endif
 
+ifeq ($(ENGINE_TARGET),YANEURAOU_2018_TNK_ENGINE_K_P)
+  ARCH_DEF += -DUSE_MAKEFILE -DYANEURAOU_2018_TNK_ENGINE_K_P
+  ENGINE_NAME := YaneuraOu2018tnk-k-p
+endif
+
+ifeq ($(ENGINE_TARGET),MATE_ENGINE)
+  ARCH_DEF += -DUSE_MAKEFILE -DMATE_ENGINE
+  ENGINE_NAME := tnk-mate
+endif
+
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
   ARCH_DEF += -DIS_64BIT -DIS_ARM -mfpu=neon
   LOCAL_ARM_NEON := true
@@ -74,7 +89,7 @@ ifeq ($(TARGET_ARCH_ABI),x86_64)
 endif
 
 ifeq ($(TARGET_ARCH_ABI),x86)
-  ARCH_DEF += 
+  ARCH_DEF +=
 endif
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
@@ -86,23 +101,27 @@ LOCAL_MODULE    := $(ENGINE_NAME)-$(TARGET_ARCH_ABI)
 LOCAL_CXXFLAGS  := -std=c++14 -fno-exceptions -fno-rtti -Wextra -Ofast -MMD -MP -fpermissive -D__STDINT_MACROS -D__STDC_LIMIT_MACROS $(ARCH_DEF)
 LOCAL_CXXFLAGS += -fPIE -Wno-unused-parameter
 LOCAL_LDFLAGS += -fPIE -pie -flto
-LOCAL_LDLIBS = 
-LOCAL_C_INCLUDES := 
+LOCAL_LDLIBS =
+LOCAL_C_INCLUDES :=
 LOCAL_CPP_FEATURES += exceptions rtti
 #LOCAL_STATIC_LIBRARIES    := -lpthread
 
 LOCAL_SRC_FILES := \
-  ../source/shogi.cpp                                                  \
+  ../source/main.cpp                                                   \
+  ../source/types.cpp                                                  \
   ../source/bitboard.cpp                                               \
   ../source/misc.cpp                                                   \
   ../source/movegen.cpp                                                \
   ../source/position.cpp                                               \
   ../source/usi.cpp                                                    \
+  ../source/usi_option.cpp                                             \
   ../source/thread.cpp                                                 \
   ../source/tt.cpp                                                     \
-  ../source/move_picker.cpp                                            \
+  ../source/movepick.cpp                                               \
+  ../source/timeman.cpp                                                \
   ../source/extra/book/apery_book.cpp                                  \
   ../source/extra/book/book.cpp                                        \
+  ../source/extra/book/makebook2019.cpp                                \
   ../source/extra/bitop.cpp                                            \
   ../source/extra/entering_king_win.cpp                                \
   ../source/extra/long_effect.cpp                                      \
@@ -111,7 +130,6 @@ LOCAL_SRC_FILES := \
   ../source/extra/mate/mate_n_ply.cpp                                  \
   ../source/extra/benchmark.cpp                                        \
   ../source/extra/test_cmd.cpp                                         \
-  ../source/extra/timeman.cpp                                          \
   ../source/extra/see.cpp                                              \
   ../source/extra/sfen_packer.cpp                                      \
   ../source/extra/kif_converter/kif_convert_tools.cpp                  \
@@ -120,27 +138,21 @@ LOCAL_SRC_FILES := \
   ../source/eval/kppt/evaluate_kppt_learner.cpp                        \
   ../source/eval/kpp_kkpt/evaluate_kpp_kkpt.cpp                        \
   ../source/eval/kpp_kkpt/evaluate_kpp_kkpt_learner.cpp                \
-  ../source/eval/kpppt/evaluate_kpppt.cpp                              \
-  ../source/eval/kpppt/evaluate_kpppt_learner.cpp                      \
-  ../source/eval/kppp_kkpt/evaluate_kppp_kkpt.cpp                      \
-  ../source/eval/kppp_kkpt/evaluate_kppp_kkpt_learner.cpp              \
-  ../source/eval/kkpp_kkpt/evaluate_kkpp_kkpt.cpp                      \
-  ../source/eval/kkpp_kkpt/evaluate_kkpp_kkpt_learner.cpp              \
-  ../source/eval/kkppt/evaluate_kkppt.cpp                              \
-  ../source/eval/kkppt/evaluate_kkppt_learner.cpp                      \
-  ../source/eval/kpp_kkpt_fv_var/evaluate_kpp_kkpt_fv_var.cpp          \
-  ../source/eval/kpp_kkpt_fv_var/evaluate_kpp_kkpt_fv_var_learner.cpp  \
   ../source/eval/evaluate.cpp                                          \
   ../source/eval/evaluate_io.cpp                                       \
   ../source/eval/evaluate_mir_inv_tools.cpp                            \
   ../source/engine/user-engine/user-search.cpp                         \
-  ../source/engine/help-mate-engine/help-mate-search.cpp               \
   ../source/engine/2018-otafuku-engine/2018-otafuku-search.cpp         \
   ../source/learn/learner.cpp                                          \
   ../source/learn/learning_tools.cpp                                   \
   ../source/learn/multi_think.cpp
 
-ifeq ($(ENGINE_TARGET),YANEURAOU_2018_TNK_ENGINE)
+ifeq ($(ENGINE_TARGET),MATE_ENGINE)
+LOCAL_SRC_FILES += \
+	../source/engine/mate-engine/mate-search.cpp
+endif
+
+ifeq ($(findstring YANEURAOU_2018_TNK_ENGINE,$(ENGINE_TARGET)),YANEURAOU_2018_TNK_ENGINE)
 LOCAL_SRC_FILES += \
   ../source/eval/nnue/evaluate_nnue.cpp                                \
   ../source/eval/nnue/evaluate_nnue_learner.cpp                        \
