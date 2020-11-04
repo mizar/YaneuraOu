1d2bc6f1e7332e3d850c8abf84fa151cd12b0a6f
Mizar <mizar.jp@gmail.com>
2020-11-04 20:53:56 +0900

    - android_build.sh: parallel build

--- script/android_build.sh
+++ script/android_build.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
 
+JOBS=`grep -c ^processor /proc/cpuinfo 2>/dev/null`
+
 pushd `dirname $0`
 pushd ..
 
 mkdir -p build/android/KPPT
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_KPPT
-ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_KPPT > >(tee build/android/KPPT/KPPT.log) || exit $?
+ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_KPPT -j${JOBS} > >(tee build/android/KPPT/KPPT.log) || exit $?
 cp libs/**/* build/android/KPPT
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_KPPT
 
 mkdir -p build/android/KPP_KKPT
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_KPP_KKPT
-ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_KPP_KKPT > >(tee build/android/KPP_KKPT/KPP_KKPT.log) || exit $?
+ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_KPP_KKPT -j${JOBS} > >(tee build/android/KPP_KKPT/KPP_KKPT.log) || exit $?
 cp libs/**/* build/android/KPP_KKPT
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_KPP_KKPT
 
 mkdir -p build/android/KOMA
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_MATERIAL
-ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_MATERIAL > >(tee build/android/KOMA/KOMA.log) || exit $?
+ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_MATERIAL -j${JOBS} > >(tee build/android/KOMA/KOMA.log) || exit $?
 cp libs/**/* build/android/KOMA
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_MATERIAL
 
 mkdir -p build/android/NNUE
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_NNUE
-ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_NNUE > >(tee build/android/NNUE/NNUE.log) || exit $?
+ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_NNUE -j${JOBS} > >(tee build/android/NNUE/NNUE.log) || exit $?
 cp libs/**/* build/android/NNUE
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_NNUE
 
 mkdir -p build/android/NNUE_HALFKPE9
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_HALFKPE9
-ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_HALFKPE9 > >(tee build/android/NNUE_HALFKPE9/NNUE_HALFKPE9.log) || exit $?
+ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_HALFKPE9 -j${JOBS} > >(tee build/android/NNUE_HALFKPE9/NNUE_HALFKPE9.log) || exit $?
 cp libs/**/* build/android/NNUE_HALFKPE9
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_HALFKPE9
 
 mkdir -p build/android/NNUE_KP256
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_KP256
-ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_KP256 > >(tee build/android/NNUE_KP256/NNUE_KP256.log) || exit $?
+ndk-build ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_KP256 -j${JOBS} > >(tee build/android/NNUE_KP256/NNUE_KP256.log) || exit $?
 cp libs/**/* build/android/NNUE_KP256
 ndk-build clean ENGINE_TARGET=YANEURAOU_ENGINE_NNUE_KP256
 
 mkdir -p build/android/MATE
 ndk-build clean ENGINE_TARGET=MATE_ENGINE
-ndk-build ENGINE_TARGET=MATE_ENGINE > >(tee build/android/MATE/MATE.log) || exit $?
+ndk-build ENGINE_TARGET=MATE_ENGINE -j${JOBS} > >(tee build/android/MATE/MATE.log) || exit $?
 cp libs/**/* build/android/MATE
 ndk-build clean ENGINE_TARGET=MATE_ENGINE
 
 mkdir -p build/android/USER
 ndk-build clean ENGINE_TARGET=USER_ENGINE
-ndk-build ENGINE_TARGET=USER_ENGINE > >(tee build/android/USER/USER.log) || exit $?
+ndk-build ENGINE_TARGET=USER_ENGINE -j${JOBS} > >(tee build/android/USER/USER.log) || exit $?
 cp libs/**/* build/android/USER
 ndk-build clean ENGINE_TARGET=USER_ENGINE
 
